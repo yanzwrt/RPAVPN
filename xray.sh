6783567e3886349c5b01f245c405fb5719e5640a
yanzwrt <165026256+yanzwrt@users.noreply.github.com>
2025-10-14 18:17:58 +0700

    xray.sh

--- xray.sh
+++ xray.sh
@@ -59,7 +59,7 @@ apt -y install chrony
 timedatectl set-ntp true
 systemctl enable chronyd && systemctl restart chronyd
 systemctl enable chrony && systemctl restart chrony
-timedatectl set-timezone Asia/Kuala_Lumpur
+timedatectl set-timezone Asia/Jakarta
 chronyc sourcestats -v
 chronyc tracking -v
 date
