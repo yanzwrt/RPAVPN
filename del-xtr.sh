82f3c2b88c2397adf558809f07f96f68ccbdc6fe
yanzwrt <165026256+yanzwrt@users.noreply.github.com>
2025-11-27 22:15:43 +0700

    del-xtr.sh

--- del-xtr.sh
+++ del-xtr.sh
@@ -2,52 +2,69 @@
 # =========================================
 # Quick Setup | Script Setup Manager
 # Edition : Stable Edition V1.0
-# Auther  : RakhaVPN
-# (C) Copyright 2022
+# Author  : RakhaVPN (mod RPAVPN)
+# (C) Copyright 2025
 # =========================================
+
 clear
 red='\e[1;31m'
 green='\e[0;32m'
+orange='\e[1;33m'
+CYAN='\e[0;36m'
 NC='\e[0m'
-NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/usr/local/etc/xray/trojan.json")
-	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
-        echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-        echo -e "\\E[0;47;30m   Delete XRAY Trojan TCP Account  \E[0m"
-        echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-		echo ""
-		echo "You have no existing clients!"
-        echo ""
-		exit 1
-	fi
-        echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-        echo -e "\\E[0;47;30m   Delete XRAY Trojan TCP Account  \E[0m"
-        echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-	
-	echo " Select the existing client you want to remove"
-	echo " Press CTRL+C to return"
+
+CONFIG_TCP="/usr/local/etc/xray/trojan.json"
+
+NUMBER_OF_CLIENTS=$(grep -c -E "^### " "$CONFIG_TCP")
+if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
     echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-	echo "     No  Expired   User"
-	grep -E "^### " "/usr/local/etc/xray/trojan.json" | cut -d ' ' -f 2-3 | nl -s ') '
-	until [[ ${CLIENT_NUMBER} -ge 1 && ${CLIENT_NUMBER} -le ${NUMBER_OF_CLIENTS} ]]; do
-		if [[ ${CLIENT_NUMBER} == '1' ]]; then
-			read -rp "Select one client [1]: " CLIENT_NUMBER
-		else
-			read -rp "Select one client [1-${NUMBER_OF_CLIENTS}]: " CLIENT_NUMBER
-		fi
-	done
-user=$(grep -E "^### " "/usr/local/etc/xray/trojan.json" | cut -d ' ' -f 2 | sed -n "${CLIENT_NUMBER}"p)
-exp=$(grep -E "^### " "/usr/local/etc/xray/trojan.json" | cut -d ' ' -f 3 | sed -n "${CLIENT_NUMBER}"p)
-sed -i "/^### $user $exp/,/^},{/d" /usr/local/etc/xray/trojan.json
-rm -f /home/vps/public_html/$user-TRTCP.yaml
+    echo -e "\\E[0;47;30m   Delete XRAY Trojan TCP Account  \E[0m"
+    echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+    echo ""
+    echo "You have no existing clients!"
+    echo ""
+    read -p "$( echo -e "Press ${orange}[ ${NC}${green}Enter${NC} ${CYAN}]${NC} Back to menu . . .") "
+    menu
+fi
+
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\\E[0;47;30m   Delete XRAY Trojan TCP Account  \E[0m"
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+
+echo " Select the existing client you want to remove"
+echo " Press CTRL+C to return"
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo "     No  Expired   User"
+grep -E "^### " "$CONFIG_TCP" | cut -d ' ' -f 2-3 | nl -s ') '
+
+until [[ ${CLIENT_NUMBER} -ge 1 && ${CLIENT_NUMBER} -le ${NUMBER_OF_CLIENTS} ]]; do
+    if [[ ${CLIENT_NUMBER} == '1' ]]; then
+        read -rp "Select one client [1]: " CLIENT_NUMBER
+    else
+        read -rp "Select one client [1-${NUMBER_OF_CLIENTS}]: " CLIENT_NUMBER
+    fi
+done
+
+user=$(grep -E "^### " "$CONFIG_TCP" | cut -d ' ' -f 2 | sed -n "${CLIENT_NUMBER}"p)
+exp=$(grep -E "^### " "$CONFIG_TCP" | cut -d ' ' -f 3 | sed -n "${CLIENT_NUMBER}"p)
+
+# Hapus blok user dari config TROJAN TCP
+sed -i "/^### $user $exp/,/^},{/d" "$CONFIG_TCP"
+
+# Hapus YAML Clash (format sama dengan add-xtr.sh: $user-$exp-TRTCP.yaml)
+rm -f /home/vps/public_html/$user-$exp-TRTCP.yaml
+
+# Restart service TROJAN TCP
 systemctl restart xray@trojan.service
+
 clear
 echo " XRAY Trojan TCP Account Deleted"
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo " Client Name : $user"
 echo " Expired On  : $exp"
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e ""
-echo -e "Script Mod By RakhaVPN"
+echo ""
+echo "Script Mod By RakhaVPN"
 echo ""
 read -p "$( echo -e "Press ${orange}[ ${NC}${green}Enter${NC} ${CYAN}]${NC} Back to menu . . .") "
 menu
